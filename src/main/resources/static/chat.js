e5e91259d4405c63b5fcd82bd17b12c407c3b978
Steaton <steaton3@gmail.com>
2017-07-11 12:31:09 +0100

    Implemented some of the basic chat front end.

--- src/main/resources/static/chat.js
+++ src/main/resources/static/chat.js
@@ -2,26 +2,47 @@ $(document).ready(function() {
     var websocket = null;
     var stompClient = null;
 
-    websocket = new SockJS('/chat');
-    stompClient = Stomp.over(websocket);
-    stompClient.connect({}, function(frame) {
-        stompClient.subscribe('/topic/chat', function(message) {
-            alert(message);
-        })
-    });
+    function initialState() {
+        $("#message").prop("disabled", true);
+        $("#sendMessage").prop("disabled", true);
+    }
 
     function sendForm() {
         stompClient.send("/topic/chat", {}, $('#message').val());
-    };
+    }
 
     function connect() {
-        var un = $('#username').val();
-        alert(un);
-        stompClient.send("/queue/connect", {}, un);
-    };
+        var username = $('#username').val();
+        websocket = new SockJS('/ws');
+        stompClient = Stomp.over(websocket);
+        stompClient.connect({username: username}, function(frame) {
+            stompClient.subscribe('/topic/chat', function(message) {
+                $("#newMessages ul").append('<li class="list-group-item">' + message.body + '</li>');
+            })
+        });
+        connected();
+    }
+
+    function connected() {
+        $("#message").prop("disabled", false);
+        $("#sendMessage").prop("disabled", false);
+        $("#username").prop("disabled", true);
+        $("#connectButton").prop("value", "Disconnect");
+    }
+
+    function sendMessage() {
+        var message = $('#message').val();
+        stompClient.send("/topic/chat", {}, message);
+    }
 
     $("#connectButton").on("click", function() {
         connect();
     })
+
+    $("#sendMessage").on("click", function() {
+        sendMessage();
+    })
+
+    initialState();
 });
 
